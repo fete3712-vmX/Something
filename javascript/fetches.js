92599ac2f6f08e6343c37bcd40932c39fcfd1ab6
xTymezzAlt <fete3712@gmail.com>
2026-07-02 14:37:09 +0200

    website

--- javascript/fetches.js
+++ javascript/fetches.js
@@ -1,7 +1,10 @@
 const url = "https://raw.githubusercontent.com/fete3712-vmX/Something/refs/heads/Website/files.json";
 async function fetchUri() {
     const response = await fetch(url);
-    if (!response.ok) {alert("Couldn't fetch files.")};
+    if (!response.ok) {
+        alert("Couldn't fetch files.")
+        return null;
+    };
     
-    return response.json()
+    return await response.json()
 }
