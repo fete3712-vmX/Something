1288871d2ea9e5ae090883ae7f26b0864e1e1fdf
xTymezzAlt <fete3712@gmail.com>
2026-07-02 14:07:40 +0200

    Delete riot/roblox/guilib directory
    Stopped working on this.

--- riot/roblox/guilib/ImageButton.lua
+++
@@ -1,7 +0,0 @@
-return {
-    createImageButton = function(image: number, callback: function, parent: Instance)
-        local imageButton = Instance.new("ImageButton",parent.Frame)
-        imageButton.Image = image
-        imageButton.MouseButton1Click:Connect(callback)
-    end
-}
