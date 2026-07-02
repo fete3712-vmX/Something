9a8d554fef83cb7786c3aee0ff400c003ae66528
xTymezzAlt <fete3712@gmail.com>
2026-07-02 15:08:22 +0200

    creator

--- javascript/creator.js
+++ javascript/creator.js
@@ -1,8 +1,14 @@
 import { fetchUri } from "./fetches";
 
 const items = fetchUri();
-async function main() {
+async function man(file,name) {
    const div = document.createElement("div");
    div.style.backgroundColor = "rgb(0,0,0)";
+   div.style.width = "160px";
+   div.style.height = "160px"
    document.appendChild(div);
+
+   const filelink = document.createElement("button");
+
 }
+
